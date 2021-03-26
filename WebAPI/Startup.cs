ae75d57fa3694436e9a2c4f1cd2e273286d4a788
Zeyneptaskin <77525793+Zeyneptaskin03@users.noreply.github.com>
2021-03-26 17:54:44 +0300

    newUpdate

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -93,7 +93,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
